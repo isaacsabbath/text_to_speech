3b9a3c4022aabbba0652c55ffdf2ebfafe7a40c7
Isaac Sabath <126468761+isaacsabbath@users.noreply.github.com>
2025-04-16 17:32:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,3 @@ from app import app
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # This allows Azure to assign the port
     app.run(host="0.0.0.0", port=port)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # use Azure-assigned port
-    app.run(host="0.0.0.0", port=port)
